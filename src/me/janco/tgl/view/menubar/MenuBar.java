78b4a6942acaeb1745b4a8ca7cee3be20e2660e9
Janco Kock <jancokock@gmail.com>
2016-03-08 23:43:24 +0100

    Removed unused actions from menubar

--- src/me/janco/tgl/view/menubar/MenuBar.java
+++ src/me/janco/tgl/view/menubar/MenuBar.java
@@ -24,27 +24,13 @@ public class MenuBar extends JMenuBar {
 	private void initialize() {
 		setLayout(new FlowLayout(80));
 		JMenu fileMenu = new JMenu("Bestand");
-		JMenu editMenu = new JMenu("Edit");
-		JMenu viewMenu = new JMenu("View");
-		JMenu helpMenu = new JMenu("Help");
 		this.add(fileMenu);
-		this.add(editMenu);
-		this.add(viewMenu);
-		this.add(helpMenu);
 
 		JMenuItem openAction = new JMenuItem("Open trimble folder");
 		JMenuItem exitAction = new JMenuItem("Afsluiten");
-		JMenuItem uploadAction = new JMenuItem("Syncroniseren met database");
-
-		// view
-		JMenuItem editorView = new JMenuItem("Editor");
-		JMenuItem agendaView = new JMenuItem("Agenda");
-		JMenuItem simulatorView = new JMenuItem("Simulator");
-		JMenuItem tableView = new JMenuItem("Table");
 
 		openAction.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
 		exitAction.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
-		uploadAction.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, ActionEvent.CTRL_MASK));
 
 		fileMenu.add(openAction);
 		openAction.addActionListener(new ActionListener() {
@@ -59,14 +45,7 @@ public class MenuBar extends JMenuBar {
 				}
 			}
 		});
-		fileMenu.add(uploadAction);
-		uploadAction.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-
-			}
-		});
-
+	
 		fileMenu.addSeparator();
 
 		fileMenu.add(exitAction);
@@ -76,39 +55,5 @@ public class MenuBar extends JMenuBar {
 				System.exit(0);
 			}
 		});
-
-		// view
-		viewMenu.add(editorView);
-		editorView.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-
-		viewMenu.add(simulatorView);
-		simulatorView.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-
-		viewMenu.add(agendaView);
-		agendaView.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-
-		viewMenu.add(tableView);
-		tableView.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
 	}
-
 }
